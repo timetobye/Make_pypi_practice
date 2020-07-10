4ab3f0990d0dc3feb79d37fceea2e5fb0b4d007c
WonTaek <time_to_bye@naver.com>
2020-07-10 23:17:10 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='intotherain',
+    version='0.0.1',
+    description='intotherain',
+    author='time_to_bye',
+    author_email='secret@secret.com',
+    url='https://github.com/timetobye/Make_pypi_practice'
+)
